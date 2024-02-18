06b02eddd86c3144877a4aeb0673545b7b261915
Edward Mercado <edward.mercado@travizory.com>
2024-02-18 23:14:10 +0800

    add application files

---
+++ app/assets/db-load-script.sql
@@ -0,0 +1,13 @@
+-- Create database
+CREATE DATABASE IF NOT EXISTS ecomdb;
+
+-- Create user and grant privileges
+CREATE USER IF NOT EXISTS 'adminuser'@'%' IDENTIFIED BY 'admin12345!';
+GRANT ALL PRIVILEGES ON ecomdb.* TO 'adminuser'@'%';
+FLUSH PRIVILEGES;
+
+USE ecomdb;
+
+CREATE TABLE products (id mediumint(8) unsigned NOT NULL auto_increment,Name varchar(255) default NULL,Price varchar(255) default NULL, ImageUrl varchar(255) default NULL,PRIMARY KEY (id)) AUTO_INCREMENT=1;
+
+INSERT INTO products (Name,Price,ImageUrl) VALUES ("Laptop","100","c-1.png"),("Drone","200","c-2.png"),("VR","300","c-3.png"),("Tablet","50","c-5.png"),("Watch","90","c-6.png"),("Phone Covers","20","c-7.png"),("Phone","80","c-8.png"),("Laptop","150","c-4.png");
